4ea640a9d16e5575b200fd84dab572e349108bc6
biyakim <w2116@e-mirim.hs.kr>
2023-04-11 19:55:59 +0900

    for문 돌리기

--- day07/tab.js
+++ day07/tab.js
@@ -1,19 +1,30 @@
 //querySelectAll('.tab-button')[0]
-$('.tab-button').eq(0).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-button').eq(0).addClass('orange')
-    $('.tab-content').eq(0).addClass('show')
-})
-$('.tab-button').eq(1).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-button').eq(1).addClass('orange')
-    $('.tab-content').eq(1).addClass('show')
-})
-$('.tab-button').eq(2).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-button').eq(2).addClass('orange')
-    $('.tab-content').eq(2).addClass('show')
-})
+var 버튼 = $('.tab-button');
+var 설명 = $('.tab-content');
+// 버튼.eq(0).on('click', function() {
+//     버튼.removeClass('orange')
+//     설명.removeClass('show')
+//     버튼.eq(0).addClass('orange')
+//     설명.eq(0).addClass('show')
+// })
+// 버튼.eq(1).on('click', function() {
+//     버튼.removeClass('orange')
+//     설명.removeClass('show')
+//     버튼.eq(1).addClass('orange')
+//     설명.eq(1).addClass('show')
+// })
+// 버튼.eq(2).on('click', function() {
+//     버튼.removeClass('orange')
+//     설명.removeClass('show')
+//     버튼.eq(2).addClass('orange')
+//     설명.eq(2).addClass('show')
+// })
+
+for(let i = 0; i<5; i++) {
+    버튼.eq(i).on('click', function() {
+        버튼.removeClass('orange')
+        설명.removeClass('show')
+        버튼.eq(i).addClass('orange')
+        설명.eq(i).addClass('show')
+    })
+}
